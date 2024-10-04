75439747b10251236161e9a83eae30219ce69aa5
Manuzinhame <115109103+Manuzinhame@users.noreply.github.com>
2024-10-04 10:49:04 -0300

    java

--- script.js
+++ script.js
@@ -40,22 +40,28 @@ const perguntas = [
 
 
 
-let atual =0;
+let atual = 0;
 let perguntaAtual;
-let historiaFinal="";
+let historiaFinal= "";
 
 function mostraPergunta(){
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
 function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas) {
+    for (const alternativa of perguntaAtual.alternativas){
+
       const botaoAlternativas = document.createElement("button");
       botaoAlternativas.textContent = alternativa.texto;
-      botaoAlternativas.addEventListener("click", function () => respostaSelecionada(alternativa));
-      caixaAlternativas.appencChild(botaoAltenativas);  
+      botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+      caixaAlternativas.appendChild(botaoAltenativas);  
      
     }
 }
